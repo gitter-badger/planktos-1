4f37b31064cde6bde08262a122c88095c911b8c7
Austin Middleton <xuset1@gmail.com>
2016-05-07 15:45:26 -0500

    Added relayed p2p messaging

--- app/js/main.js
+++ app/js/main.js
@@ -9,33 +9,12 @@ angular.module('app', ['ngMaterial'])
   $scope.messageText = '';
 
   $scope.getBlockCount = function() {
-    return Object.keys($scope.blocks).length;
   };
 
   $scope.postMessage = function() {
-    client.pushBlock($scope.board, $scope.messageText)
-      .then(() => {
-        $scope.messageText = '';
-        $scope.refresh();
-      });
   };
 
   $scope.refresh = function() {
-    client.pullBlockHashes($scope.board)
-      .then((hashes) => {
-        console.log("HASHES", hashes);
-        hashes.forEach((hash) => {
-          console.log("PULLING", hash);
-          client.pullBlock($scope.board, hash)
-            .then((block) => {
-              $timeout(() => { $scope.blocks[block.hash] = block });
-            })
-            .done();
-        });
-      })
-      .done();
   };
-
-  $scope.refresh();
 
 }]);
